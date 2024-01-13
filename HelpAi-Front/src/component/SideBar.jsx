7ad093f3e7d39b8abd4a796fa542fc182a79ba9b
jahid433 <jahidhossen02@gmail.com>
2024-01-13 23:39:16 +0300

    added required icons

---
+++ HelpAi-Front/src/component/SideBar.jsx
@@ -0,0 +1,17 @@
+import { IoGridOutline } from "react-icons/io5";
+import { FaArrowTrendUp } from "react-icons/fa6";
+import { BsChatLeft } from "react-icons/bs";
+import { IoImageOutline } from "react-icons/io5";
+import { FaMusic } from "react-icons/fa";
+import { FaRegBookmark } from "react-icons/fa";
+import { MdLogout } from "react-icons/md";
+
+const SideBar = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SideBar;
